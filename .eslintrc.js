9d84f00c5b0efea2083ed596660c7eb25746148d
Wiztim <timothyorlov@gmail.com>
2022-11-13 10:56:49 -0800

    Create ESLint config file
    This is the ESLint config file. It is the default except for the following:
    Added rules to ignore ++, double equals, and global variables

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'no-plusplus': 'off',
+    eqeqeq: 'off',
+    'no-restricted-globals': 'off',
+  },
+  ignorePatterns: ['*.test.*'],
+};
