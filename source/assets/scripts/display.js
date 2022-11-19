100f16112b230da6007bdf3816f712ae0cd5d551
Mandeep <mchera@ucsd.edu>
2022-11-19 12:12:06 -0800

    fixed linter issues and delete bug

--- source/assets/scripts/display.js
+++ source/assets/scripts/display.js
@@ -1,5 +1,3 @@
-window.addEventListener('DOMContentLoaded', init);
-
 function init ()
 {
     const imageList = JSON.parse(localStorage.getItem('Image Container'));
@@ -15,56 +13,59 @@ function init ()
             gallery.appendChild(clickable);
         }
     }
-    const clickedElement = document.querySelectorAll('a');
+    const clickedElement = document.getElementById('gallery-container').querySelectorAll('a');
     clickedElement.forEach((clickedImg) => 
     {
         clickedImg.addEventListener('click', (event) => 
         {
             event.preventDefault();
             let askUser = prompt("What would you like to do with this image: \n(1) Edit \n(2) Delete \n(3) Download");
-            if(askUser == "1" || askUser.toLowerCase() == "edit")
+            if(askUser != null)
             {
-                alert("redirecting to edit page");
-            }
-            else if(askUser == "2" || askUser.toLowerCase() == "delete")
-            {
-                const image = clickedImg.firstChild;
-                const imageList = JSON.parse(localStorage.getItem('Image Container'));
-                for(let count = 0; count < imageList.length; count++)
+                if(askUser == "1" || askUser.toLowerCase() == "edit")
                 {
-                    if(imageList[count].path == image.src)
-                    {
-                        imageList.splice(count, 1);
-                        localStorage.setItem('Image Container', JSON.stringify(imageList));
-                    }
+                    alert("redirecting to edit page");
                 }
-                location.reload();
-                alert(`The image has been successfully deleted.`);
-            }
-            else if(askUser == "3" || askUser.toLowerCase() == "download")
-            {
-                const imageList = JSON.parse(localStorage.getItem('Image Container'));
-                let pathName, fileName;
-                const image = clickedImg.firstChild;
-                for(let count = 0; count < imageList.length; count++)
+                else if(askUser == "2" || askUser.toLowerCase() == "delete")
                 {
-                    if(imageList[count].path == image.src)
+                    const image = clickedImg.firstChild;
+                    for(let count = 0; count < imageList.length; count++)
                     {
-                        fileName = imageList[count].name;
-                        pathName = imageList[count].path;
+                        if(imageList[count].path == image.src)
+                        {
+                            imageList.splice(count, 1);
+                            localStorage.setItem('Image Container', JSON.stringify(imageList));
+                        }
                     }
+                    location.reload();
+                    alert(`The image has been successfully deleted.`);
                 }
-                const download = document.createElement('a');
-                download.setAttribute('href', pathName);
-                download.setAttribute("download", fileName);
-                document.body.appendChild(download);
-                download.click();
-                download.remove();
-            }
-            else
-            {
-                alert('Please choose a valid option!');
+                else if(askUser == "3" || askUser.toLowerCase() == "download")
+                {
+                    let pathName, fileName;
+                    const image = clickedImg.firstChild;
+                    for(let count = 0; count < imageList.length; count++)
+                    {
+                        if(imageList[count].path == image.src)
+                        {
+                            fileName = imageList[count].name;
+                            pathName = imageList[count].path;
+                        }
+                    }
+                    const download = document.createElement('a');
+                    download.setAttribute('href', pathName);
+                    download.setAttribute("download", fileName);
+                    document.body.appendChild(download);
+                    download.click();
+                    download.remove();
+                }
+                else
+                {
+                    alert('Please choose a valid option!');
+                }
             }
         });
     });
 }
+
+window.addEventListener('DOMContentLoaded', init);
