591dafcf2b44bf14111f1bd0146894fdb075fb64
dquangucsd <dquang@ucsd.edu>
2022-11-22 21:43:14 -0800

    made gallery flex box resize in a row and wrap

--- source/assets/scripts/edit.js
+++ source/assets/scripts/edit.js
@@ -20,7 +20,56 @@ function init()
         {
             let image = new Image();
             image.src = imageList[count].path;
+            image.id = "editing";
+            // image.style.width = "100px";
+            // image.style.width = "100px";
             container.append(image);
+        }
+    }
+
+
+    let image = document.getElementById('editing');
+    var canvas = document.getElementById("canvas");
+    var context = canvas.getContext("2d");;
+    var cw = canvas.width;
+    var ch = canvas.height;
+
+    
+    var button = document.getElementById('rotate-button');
+    var myImage = new Image();
+    button.addEventListener('click', () =>{
+        myImage = new Image();
+        myImage.src = image.src;
+        rotate();
+    });
+
+    
+    var rotate = function () {    
+        myImage.onload = function () {
+            
+            canvas.width = myImage.height;
+            canvas.height = myImage.width;
+            cw = canvas.width;
+            ch = canvas.height;
+            //context.save();
+            
+            context.translate(cw, ch / cw);
+            context.rotate(Math.PI / 2);
+            //image.src = canvas.toDataURL("image/png");
+            
+            context.drawImage(myImage, 0, 0);  
+            for(let count = 0; count < imageList.length; count++)
+            {
+                if(imageList[count].name == imageName)
+                {
+                    //let image = new Image();
+                    image.src = canvas.toDataURL("image/png");
+                    //image.src = imageList[count].path;
+                    //image.id = "editing";
+                    container.append(image);
+                }
+            }             
+            //context.restore(); 
         }
     }
 }
