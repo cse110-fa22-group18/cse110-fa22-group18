c9b57208ddf671dea9d250a22c32ae92eab0588a
Mandeep Chera <97264598+mchera24@users.noreply.github.com>
2022-11-27 16:18:07 -0800

    Update upload.js

--- source/Exploratory-Code/upload.js
+++ source/Exploratory-Code/upload.js
@@ -1,12 +1,23 @@
 
 const parent = document.getElementById('pics')
 
-const download_button = document.getElementById('download')
+const downloadButton = document.getElementById('download')
 
 const imageList = [];
-const upload_button = document.getElementById('form');
+const uploadButton = document.getElementById('form');
 
-upload_button.addEventListener('submit', (e) => {
+function show()
+{
+    const imageList = JSON.parse(localStorage.getItem('Image Container'))
+    for (let x = 0; x < imageList.length; x++)
+    {
+        let newImg = new Image()
+        newImg.src = imageList[x].path 
+        parent.appendChild(newImg)
+    }
+}
+
+uploadButton.addEventListener('submit', (e) => {
     e.preventDefault();
     var file = new FileReader();
     const newImg = {}
@@ -15,16 +26,16 @@ upload_button.addEventListener('submit', (e) => {
         if(localStorage)
         {
             newImg.path = file.result
-            const image_list = JSON.parse(localStorage.getItem('Image Container'))
-            if(image_list == null)
+            const imageContainer = JSON.parse(localStorage.getItem('Image Container'))
+            if(imageContainer == null)
             {
                 imageList.push(newImg)
                 window.localStorage.setItem('Image Container', JSON.stringify(imageList))
             }
             else
             {
-                image_list.push(newImg)
-                window.localStorage.setItem('Image Container', JSON.stringify(image_list))
+                imageContainer.push(newImg)
+                window.localStorage.setItem('Image Container', JSON.stringify(imageContainer))
             }
             show()
         }
@@ -32,32 +43,21 @@ upload_button.addEventListener('submit', (e) => {
     file.readAsDataURL(document.getElementById('image').files[0])
 })
 
-download_button.addEventListener('click', (e) => {
+downloadButton.addEventListener('click', () => {
     download();
 })
 
 function download()
 {
-    const image_list = JSON.parse(localStorage.getItem('Image Container'))
-    const fileName = image_list[0].name
-    const pathName = image_list[0].path
+    const imageList = JSON.parse(localStorage.getItem('Image Container'))
+    const fileName = imageList[0].name
+    const pathName = imageList[0].path
     const aEl = document.createElement('a')
     aEl.setAttribute('href', pathName)
     aEl.setAttribute('download', fileName)
     document.body.appendChild(aEl)
     aEl.click();
     aEl.remove();
-}
-
-function show()
-{
-    const image_list = JSON.parse(localStorage.getItem('Image Container'))
-    for (let x = 0; x < image_list.length; x++)
-    {
-        let new_img = new Image()
-        new_img.src = image_list[x].path 
-        parent.appendChild(new_img)
-    }
 }
 
 show()
