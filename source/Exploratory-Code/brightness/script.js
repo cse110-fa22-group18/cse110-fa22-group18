35df143d541c19d6ab06f5c93434d9aa5e970c3e
Yiyu <1843044148W@gmail.com>
2022-11-30 11:32:55 -0800

    update brightness exploratory-code

--- source/Exploratory-Code/brightness/script.js
+++ source/Exploratory-Code/brightness/script.js
@@ -1,11 +1,12 @@
-window.addEventListener('DOMContentLoaded', init);
+
 
 function init() {
     const rangeInput = document.getElementById('range');
     const sampleimg = document.getElementsByClassName('sample')[0];
     console.log(sampleimg);
-    
+    let brigthSet="brightness(" + rangeInput.value + "%" +")";
     rangeInput.addEventListener("mousemove",function(){
-    sampleimg.style.filter ="brightness(" + rangeInput.value + "%" +")";
+    sampleimg.style.filter =brigthSet;
     });
 }
+window.addEventListener('DOMContentLoaded', init);
