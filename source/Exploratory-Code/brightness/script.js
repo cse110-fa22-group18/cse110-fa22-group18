3bb0593f07514147a9c678f5536feb98abffa07e
Yiyu <1843044148W@gmail.com>
2022-11-30 11:41:16 -0800

    add exploratory brightness

--- source/Exploratory-Code/brightness/script.js
+++ source/Exploratory-Code/brightness/script.js
@@ -3,7 +3,7 @@
 function init() {
     const rangeInput = document.getElementById('range');
     const sampleimg = document.getElementsByClassName('sample')[0];
-    let brigthSet="brightness(" + `${rangeInput.value}` + "%" +")";
+    let brigthSet=`brightness(${rangeInput.value}%)`;
     rangeInput.addEventListener("mousemove",function(){
     sampleimg.style.filter =brigthSet;
     });
