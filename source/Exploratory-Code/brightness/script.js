fd3fbe5e6e81b4b5be975f353e998edec73c1a48
Yiyu <1843044148W@gmail.com>
2022-11-30 11:38:30 -0800

    updates brightness exploratory-code

--- source/Exploratory-Code/brightness/script.js
+++ source/Exploratory-Code/brightness/script.js
@@ -3,8 +3,7 @@
 function init() {
     const rangeInput = document.getElementById('range');
     const sampleimg = document.getElementsByClassName('sample')[0];
-    console.log(sampleimg);
-    let brigthSet="brightness(" + rangeInput.value + "%" +")";
+    let brigthSet="brightness(" + `${rangeInput.value}` + "%" +")";
     rangeInput.addEventListener("mousemove",function(){
     sampleimg.style.filter =brigthSet;
     });
