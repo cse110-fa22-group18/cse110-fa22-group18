9ddc9e533b0d976a308db1c6ba7a0a754b50c0d5
Ira <hanabusaira@gmail.com>
2022-12-02 12:24:22 -0800

    try catch for file reading and test comments

--- source/assets/tests/brightness.test.js
+++ source/assets/tests/brightness.test.js
@@ -3,10 +3,10 @@
  * showing entire workflow, increase and decrease brightness.
  * Sources
  * - https://github.com/puppeteer/puppeteer/blob/v1.18.1/docs/api.md
- * - display.test.js done by Ira
+ * - upload-display.test.js done by Ira
  * - rotate.test.js done by Jiayi
  * 
- * run this test file using "npm test -- brightness.test.js"
+ * Run this test file using "npm test -- brightness.test.js"
  */
  const puppeteer = require("puppeteer");
  const fs = require("fs");
@@ -21,16 +21,23 @@ describe('Brightness functionality tests', () => {
         //upload images
         console.log("Uploading image(s) to gallery from " + imgDir);
         const directory = fs.opendirSync(imgDir);
-        let file = directory.readSync();
-        const uploadHandle = await page.$("input[type=file]");
-        const imgPath = imgDir + file.name;
-        await uploadHandle.uploadFile(imgPath);
-        await page.evaluate(() => document.querySelector("a[href='#upload-section']").click());
-        await page.waitForTimeout(500); // for visual confirmation of file selection
-        await page.evaluate(() => document.getElementById("upload-button").click());
-        await page.waitForTimeout(500); // timeout for upload process to complete
-        await page.evaluate(() => document.querySelector("a[href='#gallery-section']").click());
-        await page.waitForTimeout(500); // for visual confirmation of image display
+        try {
+            let file = directory.readSync();
+            const uploadHandle = await page.$("input[type=file]");
+            const imgPath = imgDir + file.name;
+            await uploadHandle.uploadFile(imgPath);
+            await page.evaluate(() => document.querySelector("a[href='#upload-section']").click());
+            await page.waitForTimeout(500); // for visual confirmation of file selection
+            await page.evaluate(() => document.getElementById("upload-button").click());
+            await page.waitForTimeout(500); // timeout for upload process to complete
+            await page.evaluate(() => document.querySelector("a[href='#gallery-section']").click());
+            await page.waitForTimeout(500); // for visual confirmation of image display
+        } catch(error)  {
+            throw error;
+        } finally {
+            directory.close();
+        }
+
         // go to edit page
         page.once('dialog', async dialog => {
             await page.waitForTimeout(500);
@@ -41,22 +48,22 @@ describe('Brightness functionality tests', () => {
         await image.click();
         await page.waitForTimeout(500);
 
-        //Move brightness slider to 200 
-        //by direcly setting range.value to 200
+        // move brightness slider to 200 
+        // by direcly setting range.value to 200
         const newInputValue = "200";
         await page.evaluate(val => document.getElementById('range').value = val, newInputValue);
         await page.waitForTimeout(500);
 
-        //click save button to save image
+        // click save button to save image
         const saveBtn= await page.$("#save-button");
         await saveBtn.click();
         await page.waitForTimeout(500);
 
-        //visual confirmation of brightness change of image on gallery-section
+        // visual confirmation of brightness change of image on gallery-section
         await page.evaluate(() => document.querySelector("a[href='#gallery-section']").click());
         await page.waitForTimeout(500); // for visual confirmation of image display
 
-        //edit it again
+        // edit it again
         page.once('dialog', async dialog => {
             await page.waitForTimeout(500);
             await dialog.accept("1");
@@ -66,21 +73,19 @@ describe('Brightness functionality tests', () => {
         await image2.click();
         await page.waitForTimeout(500);
 
-        //Move brightness slider to 30 
-        //by direcly setting range.value to 30
+        // move brightness slider to 30 
+        // by direcly setting range.value to 30
         const desInputValue = "30";
         await page.evaluate(val => document.getElementById('range').value = val, desInputValue);
         await page.waitForTimeout(500);
 
-        //click save button to save image
+        // click save button to save image
         const saveBtn2= await page.$("#save-button");
         await saveBtn2.click();
         await page.waitForTimeout(500);
 
         await page.evaluate(() => document.querySelector("a[href='#gallery-section']").click());
         await page.waitForTimeout(500); // for visual confirmation of image display
-
-
 
     }, 10000);
 
--- source/assets/tests/rotate.test.js
+++ source/assets/tests/rotate.test.js
@@ -3,7 +3,7 @@
  * - https://github.com/puppeteer/puppeteer/blob/v1.18.1/docs/api.md
  * - display.test.js done by Ira
  * 
- * run this test file using "npm test -- rotate.test.js"
+ * Run this test file using "npm test -- rotate.test.js"
  */
 const puppeteer = require("puppeteer");
 const fs = require("fs");
@@ -15,21 +15,27 @@ describe("Display functionality tests", () => {
         await page.goto("https://cse110-fa22-group18.github.io/cse110-fa22-group18/source/index.html");
     });
 
-
     it("image is properly rotated", async () => {
         console.log("Uploading image(s) to gallery from " + imgDir);
         const directory = fs.opendirSync(imgDir);
-        let file = directory.readSync();
-        const uploadHandle = await page.$("input[type=file]");
-        const imgPath = imgDir + file.name;
-        await uploadHandle.uploadFile(imgPath);
-        await page.evaluate(() => document.querySelector("a[href='#upload-section']").click());
-        await page.waitForTimeout(500); // for visual confirmation of file selection
-        await page.evaluate(() => document.getElementById("upload-button").click());
-        await page.waitForTimeout(500); // timeout for upload process to complete
-        await page.evaluate(() => document.querySelector("a[href='#gallery-section']").click());
-        await page.waitForTimeout(500); // for visual confirmation of image display
+        try {
+            let file = directory.readSync();
+            const uploadHandle = await page.$("input[type=file]");
+            const imgPath = imgDir + file.name;
+            await uploadHandle.uploadFile(imgPath);
+            await page.evaluate(() => document.querySelector("a[href='#upload-section']").click());
+            await page.waitForTimeout(500); // for visual confirmation of file selection
+            await page.evaluate(() => document.getElementById("upload-button").click());
+            await page.waitForTimeout(500); // timeout for upload process to complete
+            await page.evaluate(() => document.querySelector("a[href='#gallery-section']").click());
+            await page.waitForTimeout(500); // for visual confirmation of image display
+        } catch(error)  {
+            throw error;
+        } finally {
+            directory.close();
+        }
 
+        // input edit option in prompt box
         page.on('dialog', async dialog => {
             await page.waitForTimeout(500);
             await dialog.accept("1");
@@ -44,7 +50,7 @@ describe("Display functionality tests", () => {
         const cw = canvas.width;
         const ch = canvas.height;
 
-        //first rotate
+        // first rotate
         await rotate.click();
         await page.waitForTimeout(500);
         
@@ -53,7 +59,7 @@ describe("Display functionality tests", () => {
         expect(cw2).toBe(ch);
         expect(ch2).toBe(cw);
 
-        //second rotate
+        // second rotate
         await rotate.click();
         await page.waitForTimeout(500);
         
