14090e30b4040b2d91c7705c3f70d34a43fc7e67
Timothy Orlov <wiztim@users.noreply.github.com>
2022-12-04 22:06:19 +0000

    Automated documentation and ESLint fixing

--- source/assets/scripts/nav-toggle.js
+++ source/assets/scripts/nav-toggle.js
@@ -4,13 +4,11 @@
  * @module
  */
 function init() {
-
-    const navToggle = document.getElementsByClassName('nav-toggle')[0];
-    const navLinks = document.getElementsByClassName("nav-links")[0];
-    navToggle.addEventListener('click', ()=> {
-        navLinks.classList.toggle('active');
-    });
+  const navToggle = document.getElementsByClassName('nav-toggle')[0];
+  const navLinks = document.getElementsByClassName('nav-links')[0];
+  navToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+  });
 }
-
 
 window.addEventListener('DOMContentLoaded', init);
