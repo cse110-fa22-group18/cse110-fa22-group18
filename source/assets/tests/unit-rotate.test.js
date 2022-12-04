de2a2262cbdb2d297d148e0422a6757c3a9bb515
Hazel <54999364+JiayiGuan99@users.noreply.github.com>
2022-12-04 02:08:41 -0800

    rotate unit test

---
+++ source/assets/tests/unit-rotate.test.js
@@ -0,0 +1,7 @@
+const functions = require('../tests/rotate-for-test.js');
+
+const image = "./source/assets/test-images/elden_ring.jpeg";
+
+test('image correctly rotated', () => {
+    expect(functions.rotate(image)).toBe(undefined);
+});
