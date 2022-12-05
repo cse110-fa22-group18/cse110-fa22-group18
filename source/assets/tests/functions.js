f2a366b7065fa1bcf4852bf629b6bf77dd9ae40e
Hazel <54999364+JiayiGuan99@users.noreply.github.com>
2022-12-04 17:52:18 -0800

    to avoid linter error

--- source/assets/tests/functions.js
+++ source/assets/tests/functions.js
@@ -8,42 +8,43 @@
    * @function rotate
    */
 
- let canvas;
- let context;
- let cw;
- let ch;
- let image;
- let myImage;
- let brightnessStart;
- let brightnessEnd;
- module.exports = { rotate,setBrightness };
- function rotate(myImage){
-    myImage.onload = function () {
+let canvas;
+let context;
+let cw;
+let ch;
+let image;
+let myImage;
+let brightnessStart;
+let brightnessEnd;
+
+function rotate(myImage) {
+  myImage.onload = function () {
     canvas.width = myImage.height;
     canvas.height = myImage.width;
     cw = canvas.width;
     ch = canvas.height;
-  
+
     context.translate(cw, ch / cw);
     context.rotate(Math.PI / 2);
     context.drawImage(myImage, 0, 0);
     image.src = canvas.toDataURL('image/png');
-    }
   }
-  /**
-   * Utilizing canvas, the image's brightness is adjusted according to the position
-   * of the bar and the image is updated to reflect the changes.
-   * @function setBrightness
-   */
-   function setBrightness(myImage) {
-    //myImage = new Image();
-    //myImage.src = image.src;
-    myImage.onload=function(inputValue){
+}
+/**
+ * Utilizing canvas, the image's brightness is adjusted according to the position
+ * of the bar and the image is updated to reflect the changes.
+ * @function setBrightness
+ */
+function setBrightness(myImage) {
+  //myImage = new Image();
+  //myImage.src = image.src;
+  myImage.onload = function (inputValue) {
     canvas.width = myImage.width;
     canvas.height = myImage.height;
     context.filter = brightnessStart + inputValue + brightnessEnd;
     context.drawImage(myImage, 0, 0);
     image.src = canvas.toDataURL('image/png');
-    }
   }
+}
 
+module.exports = { rotate, setBrightness };
