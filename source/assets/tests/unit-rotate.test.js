54a7e3a832a4d776571b4c4f4f050c35854c7a9c
Yiyu <1843044148W@gmail.com>
2022-12-04 17:24:40 -0800

    update unit test

--- source/assets/tests/unit-rotate.test.js
+++
@@ -1,7 +0,0 @@
-const functions = require('../tests/rotate.js');
-
-const image = "./source/assets/test-images/elden_ring.jpeg";
-
-test('image correctly rotated', () => {
-    expect(functions.rotate(image)).toBe(undefined);
-});
